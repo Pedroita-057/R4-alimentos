5d4ff1553c0985215770f67dd63d231e2192e5d1
Pedroita-057 <ps0570732@gmail.com>
2026-09-21 19:42:54 -0300

    feat: mudando importação e roteamento de /orcamento

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,10 +1,12 @@
 
+import { useNavigate } from 'react-router-dom';
 import './hero.css'
 import logo from '../../assets/logo.jpeg';
 import image from '../../assets/Atacadistas.jpg';
 import { FaWhatsapp } from "react-icons/fa";
 
 export default function Hero() {
+    const navigate = useNavigate();
     return(
         <>
          <section className='hero'>
@@ -20,7 +22,7 @@ export default function Hero() {
 
                     
                   <div className='hero-content-buttons'>
-                      <button onClick={() => window.location.href = "/orcamento"}>SOLICITAR ORÇAMENTO</button>
+                      <button onClick={() => navigate('/orcamento')}>SOLICITAR ORÇAMENTO</button>
                       <a className="whatsapp" href="https://wa.me/5582993270963" target="_blank" rel="noreferrer"><FaWhatsapp /> FALAR NO WHATSAPP</a>
                     </div>
                 </div>
